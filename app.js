b86cf6afffb78d54b46cc2baf22d67b81ed64e37
LucasRivero01 <lucas.rivero@naranjax.com>
2022-01-18 16:31:57 -0300

    se agrega el parseo

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const { crearArchivo } = require('./calculos');
 const argv = require("process")
 const factor = process.argv
-
+    // valido si el valor ingresado es un numero
 if (isNaN(factor[2])) {
     console.log('El valor ingresado no es un numero');
     process.exit(1)
 }
-
-crearArchivo(factor[2])
+// igualmente lo parseo
+let numero1 = parseInt((factor[2]))
+crearArchivo(numero1)
     .then(nombreArchivo => console.log(nombreArchivo, 'generado'))
     .catch(err => console.log(err));
